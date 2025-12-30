4e8656050c9f03402cc0b26e45ddc3248cdc02cf
DaoAnhThien <117579985+DaoAnhThien@users.noreply.github.com>
2025-12-30 07:54:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-app.get('/user', (req, res) => {
-    const userId = req.query.id;
-    const query = "SELECT * FROM users WHERE id = '" + userId + "'";
-    db.query(query, (err, result) => {
-        res.send(result);
-    });
-});
+const password = "admin123";
