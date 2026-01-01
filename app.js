c6f906bd43df1170623ac6e8154130ee4d8baff0
DaoAnhThien <117579985+DaoAnhThien@users.noreply.github.com>
2026-01-01 16:44:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const password = "admin123555";
+const AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
+const AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+const databasePassword = "MySuperSecretPass123!";
+
+const connectionString = `postgres://admin:${databasePassword}@prod-db.example.com:5432/app`;
